04b24958e26840a7c8f032dcb0c0e5616e9ead1a
Gonzalo <garciacalvog@hotmail.com>
2023-08-03 12:22:48 -0300

    redux persist implementado

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
 import { Provider } from 'react-redux';
-/* import { PersistGate } from "redux-persist/integration/react"; */
-import { store/*  , persistor */ } from './store'
+import { PersistGate } from "redux-persist/integration/react";
+import { store , persistor } from './store'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Provider store={store}>
-        {/* <PersistGate loading={null} persistor={persistor}> */}
+        <PersistGate loading={null} persistor={persistor}>
           <App />
-        {/* </PersistGate> */}
+        </PersistGate>
       </Provider>
     </BrowserRouter>
 

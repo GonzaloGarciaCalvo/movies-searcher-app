6d26404ed313a7ea56834b1ac0dd672780d46358
Gonzalo <garciacalvog@hotmail.com>
2023-08-10 23:46:25 -0300

    cambio footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,6 @@ function Footer() {
   return (
     <footer>
       <p>Gonzalo Garcia Calvo</p>
-      <p className='mb-0'>Challenge Alkemy Trainee level - Movies searcher</p>
     </footer>
   )
 }
